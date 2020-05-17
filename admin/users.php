8015f015169361f74035cf5f6fe2ff16c40f0526
Pradip Thapa <pradipthapaofficial@gmail.com>
2020-05-18 00:12:55 +0545

    Users Display Dynamically Feature Added.

--- admin/users.php
+++ admin/users.php
@@ -1,3 +1,62 @@
-<?php 
-echo "welcome to users page";
+<?php
+require "header.php";
+require "config.php";
+$sql = "select * from user order by user_id desc";
+$res = mysqli_query($con,$sql);
 ?>
+<div id="admin-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10">
+                <h1 class="admin-heading">All Users</h1>
+            </div>
+            <div class="col-md-2">
+                <a class="add-new" href="add-user.php">add user</a>
+            </div>
+            <div class="col-md-12">
+                <?php 
+            if(mysqli_num_rows($res) > 0){
+            ?>
+                <table class="content-table">
+                    <thead>
+                        <th>S.No.</th>
+                        <th>Full Name</th>
+                        <th>User Name</th>
+                        <th>Role</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </thead>
+                    <tbody>
+                        <?php 
+                        while($row = mysqli_fetch_assoc($res)){
+                        ?>
+                        <tr>
+                            <td class='id'><?php echo $row['user_id'] ?></td>
+                            <td><?php echo $row['first_name']." ".$row['last_name'] ?></td>
+                            <td><?php echo $row['username'] ?></td>
+                            <td>
+                                <?php 
+                            if($row['role'] == 1){
+                                echo "Admin";
+                            }else{
+                                echo "Normal";
+                            }
+                            ?>
+                            </td>
+                            <td class='edit'><a href='update-user.php'><i class='fa fa-edit'></i></a></td>
+                            <td class='delete'><a href='delete-user.php'><i class='fa fa-trash-o'></i></a></td>
+                        </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+                <?php } ?>
+                <ul class='pagination admin-pagination'>
+                    <li class="active"><a>1</a></li>
+                    <li><a>2</a></li>
+                    <li><a>3</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require "header.php"; ?>
