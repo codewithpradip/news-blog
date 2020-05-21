434b78dbbf8dd0b7effddf7f29bc093445940c15
Pradip Thapa <pradipthapaofficial@gmail.com>
2020-05-22 00:26:11 +0545

    show Post on Front End & Single Page Feature Added

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2020 News | Powered by <a href="#">Code With Pradip</a></span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
