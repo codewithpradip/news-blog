3ee68b674908c7c6ebdca61cd198fabe9b49abfe
Pradip Thapa <pradipthapaofficial@gmail.com>
2020-05-22 14:41:16 +0545

    Recent Post Feature Added.

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,6 @@
+<?php 
+require "config.php";
+?>
 <div id="sidebar" class="col-md-4">
     <!-- search box -->
     <div class="search-box-container">
@@ -15,23 +18,39 @@
     <!-- recent posts box -->
     <div class="recent-post-container">
         <h4>Recent Posts</h4>
+        <?php
+        /* Calculate Offset Code */
+        $limit = 3;
+        $sql = "SELECT post.post_id, post.title, post.post_date,
+        category.category_name,post.category,post.post_img FROM post
+        LEFT JOIN category ON post.category = category.category_id
+        ORDER BY post.post_id DESC LIMIT {$limit}";
+
+        $result = mysqli_query($con, $sql) or die("Query Failed. : Recent Post");
+        if(mysqli_num_rows($result) > 0){
+          while($row = mysqli_fetch_assoc($result)) {
+      ?>
         <div class="recent-post">
-            <a class="post-img" href="">
-                <img src="images/post-format.jpg" alt="" />
+            <a class="post-img" href="single.php?id=<?php echo $row['post_id']; ?>">
+                <img src="admin/upload/<?php echo $row['post_img']; ?>" alt="" />
             </a>
             <div class="post-content">
-                <h5><a href="single.php">Lorem ipsum dolor sit amet</a></h5>
+                <h5><a href="single.php?id=<?php echo $row['post_id']; ?>"><?php echo $row['title']; ?></a></h5>
                 <span>
                     <i class="fa fa-tags" aria-hidden="true"></i>
-                    <a href='category.php'>Html</a>
+                    <a href='category.php?cid=<?php echo $row['category']; ?>'><?php echo $row['category_name']; ?></a>
                 </span>
                 <span>
                     <i class="fa fa-calendar" aria-hidden="true"></i>
-                    01 Nov, 2019
+                    <?php echo $row['post_date']; ?>
                 </span>
-                <a class="read-more" href="single.php">read more</a>
+                <a class="read-more" href="single.php?id=<?php echo $row['post_id']; ?>">read more</a>
             </div>
         </div>
+        <?php
+      }
+    }
+    ?>
     </div>
     <!-- /recent posts box -->
 </div>
